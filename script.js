5cf4f2f1f0c88d31b3f9f7b1af7bd4ab5dd3a184
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2021-12-21 15:50:05 +0600

    added maths plus multiples examples

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-alert("Hello Kyrgysztan");
+alert("Hello Kyrgysztan Altynai");
 
-var (firstName);
-var ("Altynai");
+var firstName = "Altynai"
 console.log(firstName);
+
+var lastName = "Urmatbekova"
+console.log(lastName);
+
+var middleName = "Bakytbekovna";
+console.log(middleName);
+
+var nameOfStreet = "Toktogula";
+console.log(nameOfStreet);
+
+var nameOfAnimals = "Tiger, elephant giraff";
+console.log(nameOfAnimals);
+
+var nameOfFlowers = "margaret, daisy a rose";
+console.log(nameOfFlowers);
+
+// math examples
+
+var summ = (2 + 3) * 2;
+console.log(summ);
+
+var multiple = 5 * 2 + 20 - 10;
+console.log(multiple);
+
+var square = 20 + 1;
+console.log(square);
